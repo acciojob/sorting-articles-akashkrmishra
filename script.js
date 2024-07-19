f0eb75026fa6dc535787ca3f26ad72f243250434
akashkrmishra <142215838+akashkrmishra@users.noreply.github.com>
2024-07-19 13:30:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+let list=document.querySelectorAll("#band > li");
+
+console.log(list);
+let arr=[];
+for(let i=0;i<list.length;i++){
+    arr[i]=list[i].innerHTML;
+}
+
+function strip(bandName){
+    return bandName.replace(/^(a |the |an)/i, "").trim();
+};
+
+const sortedBrands=arr.sort((a,b) => strip(a) > strip(b) ? 1: -1);
+console.log(sortedBrands);
