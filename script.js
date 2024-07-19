0dd4de352931295da27c481e795d5f93f227a213
akashkrmishra <142215838+akashkrmishra@users.noreply.github.com>
2024-07-19 13:55:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,49 @@
 //your JS code here. If required.
-let list=document.querySelectorAll("#bands > li");
+const bands = [
+  'The Plot in You',
+  'The Devil Wears Prada',
+  'Pierce the Veil',
+  'Norma Jean',
+  'The Bled',
+  'Say Anything',
+  'The Midway State',
+  'We Came as Romans',
+  'Counterparts',
+  'Oh, Sleeper',
+  'A Skylit Drive',
+  'Anywhere But Here',
+  'An Old Dog'
+];
+// function strip(bandName) {
+//   return bandName.replace(/^(a |an |the )/i, '').trim();
+// }
+// const sortedBands = bands.sort((a, b) => strip(a).localeCompare(strip(b)));
+// const bandList = document.getElementById('bands');
+// sortedBands.forEach(band => {
+//   const li = document.createElement('li');
+//   li.textContent = band;
+//   bandList.appendChild(li);
+// });
 
-console.log(list);
-let arr=[];
-for(let i=0;i<list.length;i++){
-    arr[i]=list[i].innerHTML;
-}
+
+
+
+
+
+
+
+
+// let list=document.querySelectorAll("#bands > li");
+
+// console.log(list);
+// let arr=[];
+// for(let i=0;i<list.length;i++){
+//     arr[i]=list[i].innerHTML;
+// }
 
 function strip(bandName){
     return bandName.replace(/^(a |the |an)/i, "").trim();
 };
 
-const sortedBrands=arr.sort((a,b) => strip(a) > strip(b) ? 1: -1);
+const sortedBrands=bands.sort((a,b) => strip(a) > strip(b) ? 1: -1);
 console.log(sortedBrands);
